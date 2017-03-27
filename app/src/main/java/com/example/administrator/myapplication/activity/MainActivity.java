11a7902988448269be423d6afc0d80043ae0698c
betta- <2463963246@qq.com>
2017-03-27 15:49:08 +0800

    布局优化

--- app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
@@ -3,22 +3,13 @@ package com.example.administrator.myapplication.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.administrator.myapplication.R;
-import com.example.administrator.myapplication.adapter.RippleListAdapter;
-import com.example.administrator.myapplication.view.RippleView;
-import com.example.administrator.myapplication.view.SwitchView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button bt1, bt2, bt3, bt4, bt5;
+    private Button bt1, bt2, bt3, bt4, bt5,bt6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +25,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt3 = (Button) findViewById(R.id.bt3);
         bt4 = (Button) findViewById(R.id.bt4);
         bt5 = (Button) findViewById(R.id.bt5);
+        bt6 = (Button) findViewById(R.id.bt6);
         bt1.setOnClickListener(MainActivity.this);
         bt2.setOnClickListener(MainActivity.this);
         bt3.setOnClickListener(MainActivity.this);
         bt4.setOnClickListener(MainActivity.this);
         bt5.setOnClickListener(MainActivity.this);
+        bt6.setOnClickListener(MainActivity.this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt1:
-                startActivity(ClickActivity.class);
+                startActivity(MyClickActivity.class);
                 break;
             case R.id.bt2:
                 startActivity(MySwitchActivity.class);
                 break;
             case R.id.bt3:
-                startActivity(RotateAndScaleActivity.class);
+                startActivity(MySearchNotRoundActivity.class);
                 break;
             case R.id.bt4:
                 startActivity(MyPopActivity.class);
                 break;
             case R.id.bt5:
                 startActivity(MyDiaActivity.class);
+                break;
+            case R.id.bt6:
+                startActivity(MyNewCallActivity.class);
                 break;
         }
     }
