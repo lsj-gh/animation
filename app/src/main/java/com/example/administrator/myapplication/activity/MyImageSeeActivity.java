96ce0238fa148255ed6487a9a9da372ea5ba47b8
betta- <2463963246@qq.com>
2017-03-28 16:37:36 +0800

    弹窗

--- app/src/main/java/com/example/administrator/myapplication/activity/MyImageSeeActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/MyImageSeeActivity.java
@@ -1,9 +1,15 @@
 package com.example.administrator.myapplication.activity;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.graphics.Rect;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -14,6 +20,7 @@ import com.example.administrator.myapplication.R;
 public class MyImageSeeActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView imageView, bigImageView;
     ScaleAnimation scaleAnimation;
+    int actionBarHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +35,23 @@ public class MyImageSeeActivity extends AppCompatActivity implements View.OnClic
         imageView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
         imageView.setOnClickListener(this);
         bigImageView.setOnClickListener(this);
+        Rect frame = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+       int  statusBarHeight = frame.top;
+       int  titleTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+       int  titleHeight = titleTop - statusBarHeight;
+        TypedValue tv = new TypedValue();
 
+        if (MyImageSeeActivity.this.getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+        }
     }
 
     @Override
     public void onClick(View view) {
 
 
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.iv_show:
                 showBigImage();
                 break;
@@ -46,24 +62,31 @@ public class MyImageSeeActivity extends AppCompatActivity implements View.OnClic
         }
 
     }
-    private void showBigImage(){
+
+    private void showBigImage() {
         int[] location = new int[2];
         imageView.getLocationOnScreen(location);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
         float x = location[0];
         float y = location[1];
         float i = getWindowManager().getDefaultDisplay().getWidth();
+        float i1 = dm.widthPixels;
         float j = getWindowManager().getDefaultDisplay().getHeight();
+        float j1 = dm.heightPixels;
+
         float f = (imageView.getWidth() / i);
         float l = (imageView.getHeight() / j);
-        scaleAnimation = new ScaleAnimation(f, 1f, l, 1f, imageView.getLeft() + imageView.getWidth() / 2, imageView
-                .getTop()+ imageView.getHeight() / 2);
+
+        scaleAnimation = new ScaleAnimation(f, 1f, l, 1f, imageView.getLeft()+imageView.getWidth() , imageView.getTop());
         scaleAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         scaleAnimation.setDuration(2000);
         bigImageView.setAnimation(scaleAnimation);
         bigImageView.setVisibility(View.VISIBLE);
         imageView.setVisibility(View.GONE);
     }
-    private void dismissBigImage(){
+
+    private void dismissBigImage() {
         int[] location = new int[2];
         imageView.getLocationOnScreen(location);
         float x = location[0];
@@ -72,8 +95,8 @@ public class MyImageSeeActivity extends AppCompatActivity implements View.OnClic
         float j = getWindowManager().getDefaultDisplay().getHeight();
         float f = (imageView.getWidth() / i);
         float l = (imageView.getHeight() / j);
-        scaleAnimation = new ScaleAnimation(1f, f, 1f, l, imageView.getLeft() + imageView.getWidth() / 2, imageView
-                .getTop()+ imageView.getHeight() / 2);
+//+ imageView.getWidth() / 2    + imageView.getHeight() / 2
+        scaleAnimation = new ScaleAnimation(1f, f, 1f, l, imageView.getLeft()+ imageView.getWidth() , imageView.getTop());
         scaleAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         scaleAnimation.setDuration(2000);
         bigImageView.setAnimation(scaleAnimation);
